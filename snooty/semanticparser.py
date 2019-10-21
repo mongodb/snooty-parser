c8ce858ddcbb6370c2a547114059e67dc8324dc3
Varsha Subrahmanyam <varsha.subrahmanyam@Varshas-MacBook-Pro-2.local>
2019-10-21 14:47:14 -0400

    formats

--- snooty/semanticparser.py
+++ snooty/semanticparser.py
@@ -9,9 +9,9 @@ class SemanticParser:
 
     def run(self, pages: Dict[FileId, Page]) -> Dict[str, SerializableType]:
         # Specify which transformations should be included in semantic postprocessing
-        functions: List[
-            Callable[[Dict[FileId, Page]], Dict[str, SerializableType]]
-        ] = [self.slug_title]
+        functions: List[Callable[[Dict[FileId, Page]], Dict[str, SerializableType]]] = [
+            self.slug_title
+        ]
         document: Dict[str, SerializableType] = {}
 
         for fn in functions:
@@ -21,15 +21,17 @@ class SemanticParser:
 
     def slug_title(self, pages: Dict[FileId, Page]) -> Dict[str, SerializableType]:
         # Function which returns a dictionary of slug-title mappings
-        slug_title_dict = {}
+        slug_title_dict: Dict[str, SerializableType] = {}
+
         for file_id in pages:
             index = pages[file_id]
-            slug = index.source_path
+            slug = str(index.source_path)
+
             # Get the relative path from the absolute paths of the current file and the index
             common_prefix = os.path.commonprefix([index.source_path, os.getcwd()])
             if common_prefix != "":
                 slug = os.path.relpath(index.source_path, common_prefix)
-            
+
             # Skip slug-title mapping if the file is an `includes`
             if "includes" in slug:
                 continue
@@ -40,7 +42,8 @@ class SemanticParser:
             else:
                 token = "header:: "
                 idx = index.source.find(token)
-                header = index.source[idx:].split('\n')[0]
-                title = header[len(token):]
+                header = index.source[idx:].split("\n")[0]
+                title = header[len(token) :]
             slug_title_dict[slug] = title
+
         return slug_title_dict
--- snooty/test_semanticparser.py
+++ snooty/test_semanticparser.py
@@ -1,15 +1,16 @@
 import os
 from pathlib import Path
 from dataclasses import dataclass, field
-from typing import Dict
+from typing import Dict, List
 from .parser import Project
 from . import rstparser, semanticparser
 from .util import ast_dive
 from .util_test import check_ast_testing_string
-from .types import Diagnostic, ProjectConfig, FileId, Page, Diagnostic, SerializableType, List
+from .types import Diagnostic, ProjectConfig, FileId, Page, Diagnostic, SerializableType
 from .parser import parse_rst, JSONVisitor
 
 ROOT_PATH = Path("test_data")
+
 
 @dataclass
 class Backend:
@@ -35,13 +36,26 @@ class Backend:
     def on_delete(self, page_id: FileId) -> None:
         pass
 
+
 def test() -> None:
     backend = Backend()
     with Project(Path("test_data/test_semantic_parser"), backend) as project:
         project.build()
         # Ensure that the correct pages and assets exist
         assert len(backend.metadata) == 4
-        assert backend.metadata['test_data/test_semantic_parser/source/index.txt'] == 'Some Title'
-        assert backend.metadata['test_data/test_semantic_parser/source/page1.txt'] == 'Another Title'
-        assert backend.metadata['test_data/test_semantic_parser/source/page2.txt'] == 'MongoDB Java Driver'
-        assert backend.metadata['test_data/test_semantic_parser/source/page3.txt'] == 'MongoDB Server'
+        assert (
+            backend.metadata["test_data/test_semantic_parser/source/index.txt"]
+            == "Some Title"
+        )
+        assert (
+            backend.metadata["test_data/test_semantic_parser/source/page1.txt"]
+            == "Another Title"
+        )
+        assert (
+            backend.metadata["test_data/test_semantic_parser/source/page2.txt"]
+            == "MongoDB Java Driver"
+        )
+        assert (
+            backend.metadata["test_data/test_semantic_parser/source/page3.txt"]
+            == "MongoDB Server"
+        )
