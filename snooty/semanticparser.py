3b82b2c6e7c18c833593883b7cb14384106a3915
Varsha Subrahmanyam <varsha.subrahmanyam@Varshas-MacBook-Pro-2.local>
2019-10-23 18:28:39 -0400

    adds toctree list function in semantic parser as well as some tests

--- snooty/semanticparser.py
+++ snooty/semanticparser.py
@@ -1,4 +1,4 @@
-from typing import Callable, Dict, List
+from typing import Callable, Dict, List, Any, cast
 
 from .types import FileId, Page, SerializableType
 
@@ -9,12 +9,34 @@ class SemanticParser:
 
     def run(self, pages: Dict[FileId, Page]) -> Dict[str, SerializableType]:
         # Specify which transformations should be included in semantic postprocessing
-        functions: List[
-            Callable[[Dict[FileId, Page]], Dict[str, SerializableType]]
-        ] = []
+        functions: List[Callable[[Dict[FileId, Page]], Dict[str, SerializableType]]] = [
+            self.toctree
+        ]
         document: Dict[str, SerializableType] = {}
 
         for fn in functions:
             field: Dict[str, SerializableType] = fn(pages)
             document.update(field)
         return document
+
+    def toctree(self, pages: Dict[FileId, Page]) -> Dict[str, SerializableType]:
+        nodes: List[str] = []
+        for fileid in pages:
+            page = pages[fileid]
+            ast: Dict[str, Any] = cast(Dict[str, Any], page.ast)
+            find_toctree_nodes(ast, nodes)
+        return {"toctreeNodes": nodes}
+
+
+def find_toctree_nodes(node: Dict[str, Any], nodes: List[str]) -> None:
+
+    if "children" not in node.keys():
+        return
+
+    if node["type"] == "directive":
+        if len(node["children"]) == 0 and "entries" in node.keys():
+            for toctree_node in node["entries"]:
+                nodes.append(toctree_node)
+
+    for child in node["children"]:
+        find_toctree_nodes(child, nodes)
