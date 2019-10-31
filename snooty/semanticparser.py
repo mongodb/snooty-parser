e9a6423d3c6b07bf51ec8ff3f05056143e9fc10e
Varsha Subrahmanyam <varsha.subrahmanyam@Varshas-MacBook-Pro-2.local>
2019-10-31 13:31:04 -0400

    formatting

--- snooty/semanticparser.py
+++ snooty/semanticparser.py
@@ -10,7 +10,8 @@ class SemanticParser:
     def run(self, pages: Dict[FileId, Page]) -> Dict[str, SerializableType]:
         # Specify which transformations should be included in semantic postprocessing
         functions: List[Callable[[Dict[FileId, Page]], Dict[str, SerializableType]]] = [
-            self.toctree
+            self.toctree,
+            self.slug_title
         ]
         document: Dict[str, SerializableType] = {}
 
@@ -86,3 +87,39 @@ def find_toctree_nodes(
     for child_ast in ast["children"]:
         find_toctree_nodes(fileid, child_ast, pages, node, fileid_dict)
 
+  def slug_title(self, pages: Dict[FileId, Page]) -> Dict[str, SerializableType]:
+      # Function which returns a dictionary of slug-title mappings
+      slug_title_dict: Dict[str, SerializableType] = {}
+      for file_id, page in pages.items():
+          # remove the file extension from the slug
+          slug = file_id.without_known_suffix
+
+          # Skip slug-title mapping if the file is an `includes`
+          if "includes" in slug or "images" in slug:
+              continue
+
+          # Parse for title
+          title = ""
+          ast: Dict[str, Any] = cast(Dict[str, Any], page.ast)
+          title_is_set = False
+
+          if ast is None:
+              return {}
+
+          for child in ast["children"]:
+              if title_is_set:
+                  break
+              if child["type"] == "section":
+                  for section_child in child["children"]:
+                      if title_is_set:
+                          break
+                      if section_child["type"] == "heading":
+                            for heading_child in section_child["children"]:
+                                if title_is_set:
+                                    break
+                                if heading_child["type"] == "text":
+                                    title = heading_child["value"]
+                                    title_is_set = True
+            slug_title_dict[slug] = title
+
+        return {"slugToTitle": slug_title_dict}
