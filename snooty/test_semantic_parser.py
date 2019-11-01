1871fa4b1c6091feaba6889c76bcd378e8f6d7e8
Varsha Subrahmanyam <varsha.subrahmanyam@Varshas-MacBook-Pro-2.local>
2019-11-01 15:17:51 -0400

    implementation and tests

--- snooty/test_semantic_parser.py
+++ snooty/test_semantic_parser.py
@@ -84,3 +84,42 @@ def test_toctree(backend: Backend) -> None:
         "title": "test_data",
         "slug": "/",
     }
+        # Ensure that the correct pages and assets exist for slug-title mapping
+        slugToTitle: Dict[str, str] = cast(
+            Dict[str, str], backend.metadata["slugToTitle"]
+        )
+
+        assert len(slugToTitle) == 4
+        assert slugToTitle["index"] == "Connection Limits and Cluster Tier"
+        assert slugToTitle["page1"] == "Print this heading"
+        assert slugToTitle["page2"] == "Heading is not at the top for some reason"
+        assert slugToTitle["page3"] == ""
+        
+        # Ensure that the correct pages and assets exist for toctree
+        toctree: List[Any] = cast(List[Any], backend.metadata["toctree"])
+
+        assert len(toctree) == 5
+        assert any(node["title"] == "Print this heading" for node in toctree)
+        assert any(
+            node["title"] == "Connection Limits and Cluster Tier"
+            and len(node["children"]) == 2
+            for node in toctree
+        )
+
+        # Ensure that the correct pages and assets exist for breadcrumbs
+        pages: Dict[str, Any] = cast(Dict[str, Any], backend.metadata["pages"])
+        
+        assert(len(pages) == 3)
+        assert(len(pages["page1"]) == 4)
+        assert(["index"] in pages["page1"])
+        assert(["page3"] in pages["page1"])
+        assert(["page2", "page3"] in pages["page1"])
+        assert(["index", "page2", "page3"] in pages["page1"])
+
+        assert(len(pages["page2"]) == 1)
+        assert(["index"] in pages["page2"])
+
+        assert(len(pages["page3"]) == 2)
+        assert(["index", "page2"] in pages["page3"])
+        assert(["page2"] in pages["page3"])
+        
