bba547aaace4f3ed6d026b685c4384e3e2349b1c
sophstad <sophie.stadler@mongodb.com>
2020-04-13 10:57:46 -0400

    Don't write patch_id to DB

--- snooty/main.py
+++ snooty/main.py
@@ -232,7 +232,6 @@ def _generate_build_identifiers(args: Dict[str, Optional[str]]) -> BuildIdentifi
     identifiers = {}
 
     identifiers["commit_hash"] = args["--commit"]
-    identifiers["patch_id"] = args["--patch"]
 
     return identifiers
 
