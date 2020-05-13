91b546eebb2c542b6c050c12d229c33747249471
Andrew Aldridge <i80and@foxquill.com>
2020-05-13 12:56:33 -0400

    DOP-1079: Fix crashes caused by incorrect split() usage

--- snooty/intersphinx.py
+++ snooty/intersphinx.py
@@ -124,7 +124,7 @@ class Inventory:
                 logger.debug(f"Invalid priority in intersphinx inventory: {line}")
                 continue
 
-            domain, role = domain_and_role.split(":", 2)
+            domain, role = domain_and_role.split(":", 1)
 
             # "If {dispname} is identical to {name}, it is stored as -"
             if dispname == "-":
