2aa1d5047a254ea689cccbbd787c5409220fedf6
madelinezec <mzechar@gmail.com>
2020-10-12 15:31:44 -0400

    update other ref role test

--- snooty/test_postprocess.py
+++ snooty/test_postprocess.py
@@ -2,15 +2,17 @@
    Eventually most postprocessor tests should probably be moved into this format."""
 
 from pathlib import Path
-from .util_test import make_test, check_ast_testing_string, ast_to_testing_string
+from .util_test import make_test, check_ast_testing_string
 from .types import FileId
 from .diagnostics import TargetNotFound
 
 # ensure that broken links still generate titles
 def test_broken_link() -> None:
-     with make_test(
+    with make_test(
         {
-        Path("source/param.txt"): """
+            Path(
+                "source/param.txt"
+            ): """
 =========
 $title
 =========
@@ -29,8 +31,8 @@ The :parameter:`title` stuff works
 
         page = result.pages[FileId("param.txt")]
         check_ast_testing_string(
-                    page.ast,
-                    """
+            page.ast,
+            """
 <root>
     <section>
         <heading id="title">
@@ -54,7 +56,8 @@ The :parameter:`title` stuff works
     </section>
 </root>
                 """,
-                )
+        )
+
 
 def test_case_sensitive_labels() -> None:
     with make_test(
@@ -88,7 +91,7 @@ Main Heading
     </target>
     <section>
         <heading id="main-heading"><text>Main Heading</text></heading>
-        <paragraph><ref_role domain="std" name="label" target="a-mixedcase-label"></ref_role></paragraph>
+        <paragraph><ref_role domain="std" name="label" target="a-mixedcase-label"><text>a-mixedcase-label</text></ref_role></paragraph>
         <paragraph>
             <ref_role domain="std" name="label" target="a-MixedCase-Label" fileid="['index', 'std-label-a-MixedCase-Label']">
                 <text>Main Heading</text>
