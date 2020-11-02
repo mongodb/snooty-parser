3a4f8d27032765b2c56451ae1676d87bc7e05385
Allison Reinheimer Moore <allison@mistressofinformation.com>
2020-11-02 16:00:02 -0500

    DOP-1318: add created_at field to snooty documents

--- snooty/main.py
+++ snooty/main.py
@@ -29,6 +29,7 @@ from collections import defaultdict
 from pathlib import Path, PurePath
 from typing import Any, Dict, List, Optional, Union
 from docopt import docopt
+from datetime import datetime
 
 from . import __version__, language_server
 from .parser import Project
@@ -202,6 +203,7 @@ class MongoBackend(Backend):
             "ast": page.ast.serialize(),
             "source": page.source,
             "static_assets": checksums,
+            "created_at": datetime.utcnow(),
         }
 
         if page.query_fields:
