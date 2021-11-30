bbb5781902ede1266a7cf09e990d7ec4a917b1be
Heli Aldridge <heli@heli.pet>
2021-11-30 16:34:43 -0600

    DOP-2613: Handle unicode decode errors in literalinclude

--- snooty/test_parser.py
+++ snooty/test_parser.py
@@ -703,6 +703,20 @@ for (i = 0; i &lt; 10; i++) {
     assert len(diagnostics) == 1
     assert isinstance(diagnostics[0], DocUtilsParseError)
 
+    # Test non-textual
+    page, diagnostics = parse_rst(
+        parser,
+        path,
+        """
+.. literalinclude:: /compass-explain-plan-with-index-raw-json.png
+        """,
+    )
+    page.finish(diagnostics)
+    assert len(diagnostics) == 1
+    assert [(type(d), "utf-8" in d.message) for d in diagnostics] == [
+        (CannotOpenFile, True)
+    ]
+
 
 def test_include() -> None:
     path = ROOT_PATH.joinpath(Path("test.rst"))
