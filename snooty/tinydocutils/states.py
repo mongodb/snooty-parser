2670e2d3f58431826d52aac18a760738ef3d8e03
Heli Aldridge <heli@heli.pet>
2023-03-21 12:02:42 -0400

    Remove quoted literal blocks

--- snooty/tinydocutils/states.py
+++ snooty/tinydocutils/states.py
@@ -3112,8 +3112,6 @@ class Text(RSTState):
         indented, indent, offset, blank_finish = self.state_machine.get_indented()
         while indented and not indented[-1].strip():
             indented.trim_end()
-        if not indented:
-            return self.quoted_literal_block()
         data = "\n".join(indented)
         literal_block = nodes.literal_block(data, data)
         (
@@ -3124,24 +3122,6 @@ class Text(RSTState):
         if not blank_finish:
             nodelist.append(self.unindent_warning("Literal block"))
         return nodelist
-
-    def quoted_literal_block(self) -> Sequence[nodes.ConcreteNode]:
-        assert self.state_machine.input_lines is not None
-        abs_line_offset = self.state_machine.abs_line_offset()
-        offset = self.state_machine.line_offset
-        parent_node = nodes.Element()
-        new_abs_offset = self.nested_parse(
-            self.state_machine.input_lines[offset:],
-            input_offset=abs_line_offset,
-            node=parent_node,
-            match_titles=False,
-            state_config=statemachine.StateConfiguration(
-                (QuotedLiteralBlock,),
-                "QuotedLiteralBlock",
-            ),
-        )
-        self.goto_line(new_abs_offset)
-        return parent_node.children
 
     def definition_list_item(
         self, termline: List[str]
@@ -3411,100 +3391,6 @@ class Line(SpecializedText):
         self.state_machine.previous_line(lines)
         context[:] = []
         raise statemachine.StateCorrection("Body", "text")
-
-
-class QuotedLiteralBlock(RSTState):
-
-    """
-    Nested parse handler for quoted (unindented) literal blocks.
-
-    Special-purpose.  Not for inclusion in `state_classes`.
-    """
-
-    patterns = {
-        "initial_quoted": re.compile(r"(%(nonalphanum7bit)s)" % Body.pats),
-        "text": re.compile(r""),
-    }
-    initial_transitions = ("initial_quoted", "text")
-
-    def __init__(self, state_machine: RSTStateMachine, debug: bool = False):
-        RSTState.__init__(self, state_machine, debug)
-        self.messages: List[nodes.system_message] = []
-        self.initial_lineno: Optional[int] = None
-
-    def blank(
-        self, match: Match[str], context: List[str], next_state: str
-    ) -> statemachine.TransitionResult:
-        if context:
-            raise EOFError
-        else:
-            return context, next_state, []
-
-    def eof(self, context: List[str]) -> List[str]:
-
-        if context:
-            src, srcline = self.state_machine.get_source_and_line(self.initial_lineno)
-            text = "\n".join(context)
-            literal_block = nodes.literal_block(text, text)
-            literal_block.source = src
-            literal_block.line = srcline
-            self.parent.append(literal_block)
-        else:
-            self.parent.append(
-                self.reporter.warning(
-                    "Literal block expected; none found.",
-                    line=self.state_machine.abs_line_number(),
-                )
-            )
-            # src not available, because statemachine.input_lines is empty
-            self.state_machine.previous_line()
-        self.parent.extend(self.messages)
-        return []
-
-    def indent(self, match: Match[str], context: List[str], next_state: str) -> None:
-        assert context, "QuotedLiteralBlock.indent: context should not " "be empty!"
-
-        self.messages.append(
-            self.reporter.error(
-                "Unexpected indentation.", line=self.state_machine.abs_line_number()
-            )
-        )
-        self.state_machine.previous_line()
-        raise EOFError
-
-    def initial_quoted(
-        self, match: Match[str], context: List[str], next_state: str
-    ) -> statemachine.TransitionResult:
-        """Match arbitrary quote character on the first line only."""
-        self.remove_transition("initial_quoted")
-        quote = match.string[0]
-        pattern = re.compile(re.escape(quote), re.UNICODE)
-        # New transition matches consistent quotes only:
-        self.add_transition("quoted", (pattern, self.quoted, self.__class__.__name__))
-
-        self.initial_lineno = self.state_machine.abs_line_number()
-        return [match.string], next_state, []
-
-    def quoted(
-        self, match: Match[str], context: List[str], next_state: str
-    ) -> statemachine.TransitionResult:
-        """Match consistent quotes on subsequent lines."""
-        context.append(match.string)
-        return context, next_state, []
-
-    def text(
-        self, match: Match[str], context: List[str], next_state: str
-    ) -> statemachine.TransitionResult:
-
-        if context:
-            self.messages.append(
-                self.reporter.error(
-                    "Inconsistent literal block quoting.",
-                    line=self.state_machine.abs_line_number(),
-                )
-            )
-            self.state_machine.previous_line()
-        raise EOFError
 
 
 state_classes: Sequence[Type[StateWS]] = (
