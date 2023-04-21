1ce3e5262bae3a814e514e59cc696c6c7e8fb920
Matt Meigs <matt.meigs@mongodb.com>
2023-04-21 11:19:47 -0400

    remove unnecessary test

--- snooty/test_parser.py
+++ snooty/test_parser.py
@@ -3527,30 +3527,3 @@ def test_invalid_icon() -> None:
     )
     page.finish(diagnostics)
     assert len(diagnostics) == 0
-
-
-def test_openapi_changelog() -> None:
-    path = ROOT_PATH.joinpath(Path("test.rst"))
-    project_config = ProjectConfig(ROOT_PATH, "", source="./")
-    parser = rstparser.Parser(project_config, JSONVisitor)
-
-    page, diagnostics = parse_rst(
-        parser,
-        path,
-        """
-.. openapi-changelog::
-    :api-version: 2.0
-""",
-    )
-    page.finish(diagnostics)
-    assert diagnostics == []
-    print(page.ast)
-    check_ast_testing_string(
-        page.ast,
-        """
-<root fileid="test.rst">
-    <directive name="openapi-changelog" domain="mongodb" api-version="2.0">
-    </directive>
-</root>
-""",
-    )
