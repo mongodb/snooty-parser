35ab6dce2f3b10ed034389bbf4b06e9e1f04ef0b
branberry <brandon.ly@mongodb.com>
2023-12-14 10:19:49 -0600

    [HOTFIX]: Add no caching flag for create-cache

--- snooty/main.py
+++ snooty/main.py
@@ -3,7 +3,7 @@
 Usage:
   snooty build [--no-caching] <source-path> [--output=<path>] [options]
   snooty watch [--no-caching] <source-path> [options]
-  snooty create-cache         <source-path> [options]
+  snooty create-cache [--no-caching]         <source-path> [options]
   snooty [--no-caching] language-server
 
 Options:
