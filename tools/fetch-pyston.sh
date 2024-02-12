f2cd1d1ae734d0e792147ac4fb2cd4dca8b4b4f2
Heli Aldridge <heli@heli.pet>
2024-02-12 15:23:47 -0500

    Bump dependencies & Revert back to PyInstaller

--- tools/fetch-pyston.sh
+++ tools/fetch-pyston.sh
@@ -7,7 +7,5 @@ if [ ! -d "${EXTRACTED_PATH}" ]; then
     curl -OL "https://github.com/pyston/pyston/releases/download/pyston_${VERSION}/pyston_${VERSION}_portable_amd64.tar.gz"
     tar -xf "pyston_${VERSION}_portable_amd64.tar.gz"
     rm "pyston_${VERSION}_portable_amd64.tar.gz"
-    ln -s "libpython3.8-pyston2.3.so" "pyston_${VERSION}/lib/libpython3.8.so"
-    ln -s "libpython3.8-pyston2.3.so" "pyston_${VERSION}/lib/libpython3.8.so.1.0"
 fi
 echo "${EXTRACTED_PATH}"
