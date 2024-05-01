29bafc97c973ec04dc91ce25aa5bfe32e21b404a
Maya Raman <maya.raman19@gmail.com>
2024-05-01 10:05:22 -0400

    fixed i think

--- snooty/parser.py
+++ snooty/parser.py
@@ -716,8 +716,11 @@ class JSONVisitor:
             bullet_list = node.children[0]
             if "header-rows" in options:
                 if options['header-rows'] == len(bullet_list.children):
-                    self.diagnostics.append(InvalidTableStructure("List table cannot have only header rows", node.get_line() + len(node.children) - 1))
-                    return None
+                    print("BULLET LIST CHILDREN FOR BAD ROW")
+                    for child in bullet_list.children:
+                        print(child)
+                    self.diagnostics.append(InvalidTableStructure("List-table cannot have only header rows", node.get_line() + len(node.children) - 1))
+                    raise tinydocutils.nodes.SkipNode()
             for list_item in bullet_list.children:
                 if expected_num_columns == 0 and list_item.children:
                     expected_num_columns = len(list_item.children[0].children)
