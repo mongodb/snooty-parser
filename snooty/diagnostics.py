56e2dcaf8d19247ec49d5d0da3c01e522fcbd839
rayangler <27821750+rayangler@users.noreply.github.com>
2024-08-08 16:40:40 -0400

    Add list of available ids to diagnostic

--- snooty/diagnostics.py
+++ snooty/diagnostics.py
@@ -968,11 +968,12 @@ class UnknownWayfindingOption(Diagnostic):
     def __init__(
         self,
         invalid_id: str,
+        valid_ids: AbstractSet[str],
         start: Union[int, Tuple[int, int]],
         end: Union[None, int, Tuple[int, int]] = None,
     ):
         super().__init__(
-            f"Wayfinding option id {invalid_id} is not valid.",
+            f"Wayfinding option id {invalid_id} is not valid. Expected one of the following: {sorted(valid_ids)}",
             start,
             end,
         )
--- snooty/parser.py
+++ snooty/parser.py
@@ -698,8 +698,11 @@ class JSONVisitor:
                 continue
 
             if not option_id in expected_options_dict:
+                available_ids = set(expected_options_dict.keys())
                 self.diagnostics.append(
-                    UnknownWayfindingOption(option_id, child_line_start)
+                    UnknownWayfindingOption(
+                        option_id, available_ids, child_line_start
+                    )
                 )
                 continue
 
